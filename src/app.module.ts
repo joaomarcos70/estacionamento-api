aa7ac3a2f43f9c4d466de023a17c8c5398a93cb0
joão <joaomarcos70outlook.com>
2021-06-11 18:05:42 -0300

    adicionando a controller de estabelecimento no app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
+import { EstablishmentController } from './controllers/establishment.controller';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, EstablishmentController],
   providers: [AppService],
 })
 export class AppModule {}
